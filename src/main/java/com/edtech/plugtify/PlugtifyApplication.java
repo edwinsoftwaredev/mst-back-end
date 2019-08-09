afbe21cfc27a0db6d54382122a1d4e938ec2afad
Edwin <edwin_m_paz@hotmail.com>
2019-08-08 21:37:29 -0600

    Initial Commit

---
+++ src/main/java/com/edtech/plugtify/PlugtifyApplication.java
@@ -0,0 +1,13 @@
+package com.edtech.plugtify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugtifyApplication.class, args);
+	}
+
+}

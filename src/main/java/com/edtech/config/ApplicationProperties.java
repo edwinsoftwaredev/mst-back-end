0a7d281ee7e4576e34bdeba56ba7e5f10fde2dc4
Edwin <edwin_m_paz@hotmail.com>
2019-08-11 19:58:57 -0600

    Create ApplicationProperties.java
    Externalizing configurations properties that are in application.yaml file

---
+++ src/main/java/com/edtech/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.edtech.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}

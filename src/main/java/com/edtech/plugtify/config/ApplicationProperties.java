3365a81f2e37728163f51174deb434ee5d717170
Edwin <edwin_m_paz@hotmail.com>
2019-08-13 13:25:35 -0600

    Zalando problem configuration
    Adding the library for customization of API error

--- src/main/java/com/edtech/plugtify/config/ApplicationProperties.java
+++ src/main/java/com/edtech/plugtify/config/ApplicationProperties.java
@@ -1,12 +1,29 @@
 package com.edtech.plugtify.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+    /**
+     * ObjectMapper Bean configuration requiered by Zalando Problem.
+     * Zalando Problem is a small library to let customize APIs errors.
+     * implementation: https://www.baeldung.com/problem-spring-web
+    **/
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModules(
+                new ProblemModule(),
+                new ConstraintViolationProblemModule()
+        );
+    }
 
 }

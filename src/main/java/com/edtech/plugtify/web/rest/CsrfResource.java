b583a7f2938b975c9ff0223e48df7793941109bd
Edwin <edwin_m_paz@hotmail.com>
2019-08-14 13:34:11 -0600

    Egress default test

--- src/main/java/com/edtech/plugtify/web/rest/CsrfResource.java
+++ src/main/java/com/edtech/plugtify/web/rest/CsrfResource.java
@@ -1,9 +1,11 @@
 package com.edtech.plugtify.web.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +13,19 @@ public class CsrfResource {
 
     @GetMapping("/csrf-token")
     public CsrfToken getCsrfToken(CsrfToken csrfToken) {
+
+        System.out.println("-----RestTemplate Result------");
+
+        try {
+            // testing request with egress default
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> res = restTemplate.getForEntity("https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200", String.class);
+
+            System.out.println(res.getBody());
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+
         return csrfToken;
     }
 

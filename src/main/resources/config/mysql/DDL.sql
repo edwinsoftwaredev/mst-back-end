bfa5789732e6ad9423c1f995c5664881cc89ed17
Edwin <edwin_m_paz@hotmail.com>
2019-08-17 13:17:30 -0600

    Update DDL.sql

--- src/main/resources/config/mysql/DDL.sql
+++ src/main/resources/config/mysql/DDL.sql
@@ -8,4 +8,24 @@ create table tbl_user(
     last_modified_by varchar(50),
     last_modified_date timestamp,
     constraint pk_user primary key(id)
-)
+);
+
+create table tbl_token(
+    id bigint(20) auto_increment,
+    access_token varchar(254),
+    token_type varchar(254),
+    scope varchar(254),
+    expires_in integer,
+    refresh_token varchar(254),
+    constraint pk_token primary key(id)
+);
+
+alter table tbl_user
+add column token_id bigint(20);
+
+alter table tbl_user
+add constraint fk_token foreign key(token_id) references tbl_token(id);
+
+alter table tbl_user
+add column has_token boolean;
+

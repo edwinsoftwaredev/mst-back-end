0c20749d89e299c09105107c95237ec9b32159a6
Edwin <edwin_m_paz@hotmail.com>
2019-08-21 11:30:45 -0600

    Update SpotifyService.java

--- src/main/java/com/edtech/plugtify/service/SpotifyService.java
+++ src/main/java/com/edtech/plugtify/service/SpotifyService.java
@@ -1,7 +1,10 @@
 package com.edtech.plugtify.service;
 
+import com.edtech.plugtify.config.ApplicationProperties;
 import com.edtech.plugtify.domain.User;
 import com.edtech.plugtify.service.dto.AuthorizationCodeDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -10,12 +13,26 @@ import org.springframework.web.client.RestTemplate;
 @Transactional
 public class SpotifyService {
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private final String SPOTIFY_TOKEN_END_POINT = "https://accounts.spotify.com/api/token";
 
-    public SpotifyService() { }
+    private RestTemplate restTemplate = new RestTemplate();
+    private ApplicationProperties applicationProperties;
+
+    public SpotifyService(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
 
     public User processAuthorizationCode(AuthorizationCodeDTO authorizationCode) {
-        this.restTemplate.post
+
+        String value = ""
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", value);
+
+        HttpEntity<AuthorizationCodeDTO> authorizationCodeDTOHttpEntity =
+            new HttpEntity<>(authorizationCode, httpHeaders);
+
+        this.restTemplate.postForEntity(this.SPOTIFY_TOKEN_END_POINT, authorizationCodeDTOHttpEntity, );
     }
 
 }
